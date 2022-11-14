15c006ddcfa183ae7c08a477677df38ffd9b604a
Saeed Karim <s.karim708@gmail.com>
2022-11-13 19:15:47 -0600

    added Project06

---
+++ src/homeworks/Homework08.java
@@ -0,0 +1,97 @@
+package homeworks;
+
+import utilities.ScannerHelper;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Homework08 {
+    public static void main(String[] args) {
+        System.out.println("---Task 1---");
+        System.out.println(countConsonants("hello"));
+
+
+        System.out.println("\n---Task 2---");
+        System.out.println(wordArray("Hello, nice to meet you!!"));
+
+
+        System.out.println("\n---Task 3---");
+        System.out.println(removeExtraSpaces("java is  fun"));
+
+
+        System.out.println("\n---Task 4---");
+        System.out.println(count3OrLess());
+
+
+        System.out.println("\n---Task 5---");
+        System.out.println(isDateFormatValid("1/21/1999"));
+
+
+        System.out.println("\n---Task 6---");
+        System.out.println(isEmailFormatValid("aj@gmail.com"));
+
+    }
+    //Task 1
+    public static int countConsonants(String str){
+        str = str.replaceAll("[^A-Za-z]", "");
+        str = str.replaceAll("[AEIOUaeiou]", "");
+
+        return str.length();
+    }
+
+    //Task 2
+    public static String wordArray(String str){
+        str = removeExtraSpaces(str);
+        str = str.replaceAll("[^A-Za-z ]", "");
+        return Arrays.toString(str.split(" "));
+    }
+
+    //Task 3
+    public static String removeExtraSpaces(String str){
+        str = str.replaceAll("[\\s]{1,}", " ");
+        return str;
+    }
+
+    //Task 4
+    public static int count3OrLess(){
+        String str = ScannerHelper.getAString();
+        int counter = 0;
+
+        str = " " + str + " ";
+
+        str = str.replaceAll("[^A-Za-z ]", " ");
+
+        Pattern pattern = Pattern.compile(" [A-Za-z]{1,3} ");
+        Matcher match = pattern.matcher(str);
+
+        while(match.find()){
+            counter++;
+        }
+        /* Other Way of completing this task EXPLAIN THIS WAY FOR HELPING PEOPLE
+        str = str.replaceAll("[^A-Za-z ]", "");
+        String[] arr = str.split(" ");
+
+        for (String s : arr) {
+            if(s.length() <= 3) counter++;
+        }
+         */
+        return counter;
+    }
+
+    //Task 5
+    public static boolean isDateFormatValid(String str){
+        Pattern pattern = Pattern.compile("[0-9]{2}/[0-9]{2}/[0-9]{4}");
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.matches();
+    }
+
+    //Task 6
+    public static boolean isEmailFormatValid(String str){
+        Pattern pattern = Pattern.compile("[\\w.]{2,}@[\\w.[^_]]{2,}\\.[\\w[^_]]{2,}");
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.matches();
+    }
+}
---
+++ src/projects/Projects06.java
@@ -0,0 +1,105 @@
+package projects;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class Projects06 {
+    public static void main(String[] args) {
+
+
+        System.out.println("\n-----------TASK 1-----------\n");
+        String[] str = {"Hi Saeed", "I love Java", " Suayip",  " "};
+        countMultipleWords(str);
+
+
+        System.out.println("\n-----------TASK 2-----------\n");
+        ArrayList<Integer> nums1 = new ArrayList<>(Arrays.asList(1,2,3,-4,-5,6,-7,-8));
+        removeNegatives(nums1);
+
+        System.out.println("\n-----------TASK 3-----------\n");
+        validatePassword("Saeed@123");
+
+        System.out.println("\n-----------TASK 4-----------\n");
+        System.out.println(emailIsValid("JavaIsGr8et@school.com"));
+
+
+
+
+
+
+    }
+
+
+    public static void countMultipleWords(String[] arrayName) {
+
+        //TASK 1
+        int count = 0;
+        for (String words : arrayName) {
+            for (int i = 1; i < words.length() -1 ; i++) {
+                if (words.charAt(i) == ' ' & Character.isLetter(words.charAt(i - 1)) & Character.isLetter(words.charAt(i + 1))){
+                    count++;
+                    break;
+            }
+
+            }
+        }
+        System.out.println(count);
+    }
+    //TASK 2
+    public static void removeNegatives(ArrayList<Integer> nums){
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) < 0 ){
+                nums.remove(nums.get(i));
+                i--;
+            }
+
+        }
+        System.out.println(nums);
+
+    }
+
+    //TASK 3
+    public static void validatePassword(String str){
+
+        if (str.length() >= 8 && str.length() <= 16){
+            int countDigit = 0, countUpper = 0, countLower = 0, countSpecial = 0;
+            for (int i = 0; i < str.length(); i++){
+                if (Character.isUpperCase(str.charAt(i))){
+                    countUpper++;
+                } else if (Character.isLowerCase(str.charAt(i))) {
+                    countLower++;
+                } else if (Character.isDigit(str.charAt(i))) {
+                    countDigit++;
+                } else if (str.charAt(i) != ' '){
+                    countSpecial++;
+                }
+            }if (countLower > 0 & countUpper > 0 & countDigit > 0 & countSpecial > 0){
+                System.out.println(true);
+            }else System.out.println(false);
+        }
+    }
+    //TASK 4
+
+    public static boolean emailIsValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+
+        }
+
+
+
+
+
+
+
